e49ce7d5920c8568018128e982ea648d1dcecc49
Vincentzyx <929403983@qq.com>
2020-04-28 03:27:26 +0800

    Implementing ws function.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import SocketIO from 'socket.io-client'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        HelloWorld
+    },
+    data() {
+        return {
+            socket: {}
+        }
+    },
+    mounted() {
+        this.socket = SocketIO("http://localhost:8081/");
+        this.socket.on("message", function(msg) {
+            console.log(msg);
+            console.log("test");
+        });
+        setInterval(()=>{
+            this.socket.emit("message", "connected from client");
+        }, 1000);
+    }
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+
+const routes = [
+    {
+        path: "/",
+        components: ()=>import("@/components/HelloWorld.vue")
+    }
+]
+
+const router = new VueRouter({
+    routes
+})
+
+
 new Vue({
-  render: h => h(App),
+    router,
+    render: h => h(App),
 }).$mount('#app')
